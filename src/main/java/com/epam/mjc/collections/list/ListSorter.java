67559a02038c2bd07b062cedd84088c310d843d3
Ziyosh <ziyodaakhmedova1998@gmail.com>
2022-11-21 02:43:10 +0300

    My List commit

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
-
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int i = Math.abs(Integer.parseInt(a)) - Math.abs(Integer.parseInt(b));
+        return i==0&&a.startsWith("-")?-1:(i==0&&b.startsWith("-"))?1:i;
     }
 }
